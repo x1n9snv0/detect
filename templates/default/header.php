6f58f633980f09642fdb73d51ff336380856aada
x1n9snv0 <vichi_7@sina.com>
2018-10-19 23:21:18 +0800

    1.header模板中引入LULUUI的全部JS文件

--- templates/default/header.php
+++ templates/default/header.php
@@ -12,7 +12,29 @@
     <link rel="stylesheet" type="text/css" href="css/common/ui.css">
     <link rel="stylesheet" type="text/css" href="templates/default/style.css">
     <script type="text/javascript" src="js/plugin/jquery.js"></script>
-    <script type="text/javascript" src="js/common/ui/*.js"></script>
+    <script type="text/javascript" src="js/common/ui/Checkbox.js"></script>
+    <script type="text/javascript" src="js/common/ui/Color.js"></script>
+    <script type="text/javascript" src="js/common/ui/Datalist.js"></script>
+    <script type="text/javascript" src="js/common/ui/DateTime.js"></script>
+    <script type="text/javascript" src="js/common/ui/Dialog.js"></script>
+    <script type="text/javascript" src="js/common/ui/Drop.js"></script>
+    <script type="text/javascript" src="js/common/ui/DropList.js"></script>
+    <script type="text/javascript" src="js/common/ui/DropPanel.js"></script>
+    <script type="text/javascript" src="js/common/ui/Enhance.js"></script>
+    <script type="text/javascript" src="js/common/ui/ErrorTip.js"></script>
+    <script type="text/javascript" src="js/common/ui/Follow.js"></script>
+    <script type="text/javascript" src="js/common/ui/Keyboard.js"></script>
+    <script type="text/javascript" src="js/common/ui/LightTip.js"></script>
+    <script type="text/javascript" src="js/common/ui/Loading.js"></script>
+    <script type="text/javascript" src="js/common/ui/Checkbox.js"></script>
+    <script type="text/javascript" src="js/common/ui/Pagination.js"></script>
+    <script type="text/javascript" src="js/common/ui/Placeholder.js"></script>
+    <script type="text/javascript" src="js/common/ui/Radio.js"></script>
+    <script type="text/javascript" src="js/common/ui/Range.js"></script>
+    <script type="text/javascript" src="js/common/ui/Select.js"></script>
+    <script type="text/javascript" src="js/common/ui/Tab.js"></script>
+    <script type="text/javascript" src="js/common/ui/Tips.js"></script>
+    <script type="text/javascript" src="js/common/ui/Validate.js"></script>
 </head>
 
 
